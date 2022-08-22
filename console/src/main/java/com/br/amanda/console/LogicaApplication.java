3324e3159dbed6e25fb40fe212492edca247ad0f
Amanda Pitta <manpitta@gmail.com>
2022-08-22 11:58:48 -0300

    projeto spring no console

---
+++ console/src/main/java/com/br/amanda/console/LogicaApplication.java
@@ -0,0 +1,18 @@
+package com.br.amanda.console;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogicaApplication {
+	public static void main(String[] args) {
+		System.out.println("Bem vindo ao mundo java!");
+	}
+
+
+//	public static void main(String[] args) {
+//		SpringApplication.run(LogicaApplication.class, args);
+//	}
+
+
+}
